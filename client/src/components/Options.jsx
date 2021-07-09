84eb55e082c96f764e5257ad55cb6a82f3c16f0e
halk_lenson@hotmail.com <leandroasilva>
2021-07-09 17:16:02 -0300

    final

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -50,6 +50,7 @@ const Options = ({ children }) => {
             <Grid item xs={12} md={6} className={classes.padding}>
               <Typography gutterBottom variant="h6">Account Info</Typography>
               <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+                {console.log(me)}
               <CopyToClipboard text={me} className={classes.margin}>
                 <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
                   Copy Your ID
